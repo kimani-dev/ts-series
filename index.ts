0af3c5099dbe3045a61cbfc0adf5b3a4a6b9300f
David Kimani <daviemim07@gmail.com>
2024-06-11 23:40:53 +0300

    generics

--- index.ts
+++ index.ts
@@ -1,77 +1,32 @@
-// Introduction to Typescript
-// primitive types
-let myName: string = "Kimani";
-let age: number = 53;
-let isMarried: boolean = false;
+// Generics
 
-// reference types
-let names: number[] = [1, 2, 3];
-let person: {
-  name: string;
-  location: string;
-} = {
-  name: "Kimani",
-  location: "TRM",
-};
-
-let amount: number | string = 2000;
-
-let something: any = null;
-something = 23;
-
-let numstrings: (number | string | boolean)[] = [1, 2, 3, "kimani", false];
-
-interface Person {
-  name: string;
-  age: number;
-  location: string;
+//API
+// expected response from an API
+interface APIResponse<T> {
+  data: T;
+  statusCode: number;
+  error: object | null;
 }
 
-type User = {
-  username: string;
+// /users
+interface User {
+  name: string;
   email: string;
-};
-
-const user: Person = {
-  name: "David Kimani",
-  age: 53,
-  location: "Westlands",
-};
-
-const admin: User = {
-  email: "soupnazi",
-  username: "soupnazi@gmail.com",
-};
-
-// inherit types and optional properties
-interface SuperAdmin extends Person {
-  phone?: string;
 }
 
-const superAdmin: SuperAdmin = {
-  name: "Klara Njeri",
-  age: 53,
-  location: "Ushago",
+const userResponse: APIResponse<User[]> = {
+  data: [{ name: "Klara Kimani", email: "klara@kimani.com" }],
+  statusCode: 200,
+  error: null,
 };
 
-// an array of a certain type
-const users: User[] = [{ email: "user1@gmail.com", username: "user1" }];
-users.push(admin);
-
-// typing functions
-function add(x: number, y: number): number {
-  return x + y;
+// /order
+interface Order {
+  userID: number;
+  meal: string;
 }
-
-add(2, 3);
-
-function fullName(first: string, last: string): string {
-  return `${first} ${last}`;
-}
-
-fullName("Klara", "Njeri");
-
-function changeEmail(): User {
-  admin.email = "change.admin@gmail.com";
-  return admin;
-}
+const orderResponse: APIResponse<Order[]> = {
+  data: [{ userID: 32, meal: "chicken tikka pizza" }],
+  statusCode: 200,
+  error: null,
+};
