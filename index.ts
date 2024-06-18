d38edc3b2115c3735c09a16fbf54ef911b5bedd7
David Kimani <daviemim07@gmail.com>
2024-06-18 23:39:24 +0300

    inheritance and access modifiers

--- index.ts
+++ index.ts
@@ -1,20 +1,44 @@
 // OOP
 class User {
-  // access modifiers (abstraction)
+  // access modifiers (abstraction) - public, private, protected
   public name: string;
-  public age: number;
+  private age: number;
+  protected location: string;
 
   constructor(name: string, age: number) {
     this.name = name;
     this.age = age;
   }
 
-  sayHello(): void {
+  public sayHello(): void {
     console.log(`${this.name} says hello`);
+  }
+
+}
+
+// inheritance
+class SuperAdmin extends User {
+  public rank: string;
+
+  monitorSystem() {
+    console.log("Monitoring the system...");
+  }
+
+  constructor(name: string, age: number, rank: string) {
+    super(name, age);
+    this.rank = rank;
   }
 }
 
-const student = new User("Kimani", 16);
-const teacher = new User("Bill Gates", 10);
-console.log(student.name); // Kimani
-console.log(teacher.name); // Bill Gates
+const admin: SuperAdmin = new SuperAdmin("Klara Kimani", 52, "Management");
+
+const fullName: String = new String(12344);
+const age: Number = new Number(2344);
+const isLoggedIn: Boolean = new Boolean(false);
+const date = new Date();
+
+console.log(date.getFullYear()); // 2024
+
+function add() {
+  return 1 + 2;
+}
